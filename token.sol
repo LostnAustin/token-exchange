28533fdb96140e1d77736441e105a54803f7b508
Chad Lengefeld <36778876+LostnAustin@users.noreply.github.com>
2022-10-26 01:29:42 -0500

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.5.2;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol';
+
+
+contract Token is ERC20Mintable {
+    string public name = "Chainlink Fall Hackathon 2022";
+    string public symbol = "CFH";
+    uint8 public decimals = 2; 
+}
